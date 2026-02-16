566ee2e4461683e83b3bc49522cdb33f78d755e6
Aswini Kumar Jena <akj.sites@gmail.com>
2026-02-16 22:41:15 +0530

    Add files via upload

---
+++ A1pi2_getWay1/src/main/java/com/example/demo/A1pi2GetWay1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A1pi2GetWay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A1pi2GetWay1Application.class, args);
+	}
+
+}
